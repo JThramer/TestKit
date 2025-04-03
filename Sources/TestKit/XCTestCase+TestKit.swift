cd02de30a1dc1bca82d8f2a188c753c16146e461
Jerrad Thramer <Jerrad.Thramer@gmail.com>
2025-04-03 01:38:56 -0600

    Adding custom path support and more generic expectations

--- Sources/TestKit/XCTestCase+TestKit.swift
+++ Sources/TestKit/XCTestCase+TestKit.swift
@@ -11,6 +11,9 @@ import XCTest
 public extension XCTestCase {
     
     var app: XCUIApplication { return .shared }
+    typealias PartialElementPath = PartialKeyPath<XCUIElement>
+    typealias ElementPath<T> = KeyPath<XCUIElement, T>
+    typealias BooleanElementPath = ElementPath<Bool>
     
     
     func wait(for interval: TimeInterval, because reason: String? = nil) {
@@ -18,7 +21,7 @@ public extension XCTestCase {
         expectation.isInverted = true
         let waiter = XCTWaiter()
         let reasonExists = !(reason?.isEmpty ?? true)
-        let becauseReason = reasonExists ? "(reason: \(reason ?? ""))" : ""
+        let becauseReason = reasonExists ? "(reason: \(reason ?? ""))" : "<none>"
         _ = XCTContext.runActivity(named: "Manually waiting for \(interval) seconds \(becauseReason)...") { _ in
             waiter.wait(for: [expectation], timeout: interval)
         }
@@ -27,13 +30,16 @@ public extension XCTestCase {
     
     func expectChange(
         in element: XCUIElement,
+        path: PartialElementPath?,
         timeout: TimeInterval = 5.0,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
-        let oldValue = element.valueAsString
+        let keyPath: PartialElementPath = path ?? \.valueAsString
+        let oldValue = element[keyPath: keyPath]
         let predicate = NSPredicate(
-            format: "%K != %@", argumentArray: [#keyPath(XCUIElement.valueAsString), (oldValue as Any)])
+            format: "%K != %@", argumentArray: [keyPath, (oldValue as Any)]
+        )
         
         let expectation = expectation(for: predicate, evaluatedWith: element)
         runActivity(
@@ -42,7 +48,8 @@ public extension XCTestCase {
             timeout: timeout,
             action: "change",
             file: file,
-            line: line)
+            line: line
+        )
         
     }
     
@@ -85,7 +92,8 @@ public extension XCTestCase {
                 timeout: timeout,
                 action: "appear",
                 file: file,
-                line: line)
+                line: line
+            )
             
         }
     
@@ -111,7 +119,8 @@ public extension XCTestCase {
             timeout: timeout,
             action: "disappear",
             file: file,
-            line: line)
+            line: line
+        )
     }
     
     func expectTapability(
@@ -120,17 +129,79 @@ public extension XCTestCase {
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
+    
+        expect(
+            \.isHittable,
+             in: element,
+             timeout: timeout,
+             description: "is tappable",
+             file: file,
+             line: line
+        )
+    }
+    
+    func expect(
+        _ condition: BooleanElementPath,
+        in element: XCUIElement,
+        toBe value: Bool? = nil,
+        timeout: TimeInterval = 5.0,
+        description actionDescription: String? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let expectedValue: Bool = value ?? true
+        expect(
+            condition,
+            in: element,
+            toBe: expectedValue,
+            timeout: timeout,
+            description: actionDescription,
+            file: file,
+            line: line
+        )
+        
+        
+        
+        
         let predicate = NSPredicate(
-            format: "%K == true", argumentArray: [#keyPath(XCUIElement.isHittable)])
+            format: "%K == \(expectedValue)",
+            argumentArray: [condition]
+        )
         
         let expectation = expectation(for: predicate, evaluatedWith: element)
         runActivity(
             with: expectation,
             on: element.description,
             timeout: timeout,
-            action: "be tappable",
+            action: actionDescription ?? "for \(condition)) to be \(expectedValue)",
             file: file,
-            line: line)
+            line: line
+        )
+    }
+    
+    func expect<T: Equatable>(
+        _ path: ElementPath<T>,
+        in element: XCUIElement,
+        toBe expectedValue: T,
+        timeout: TimeInterval = 5.0,
+        description actionDescription: String? = nil,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let predicate = NSPredicate(
+            format: "%K == \(expectedValue)",
+            argumentArray: [path]
+        )
+        
+        let expectation = expectation(for: predicate, evaluatedWith: element)
+        runActivity(
+            with: expectation,
+            on: element.description,
+            timeout: timeout,
+            action: actionDescription ?? "for \(path) to be \(expectedValue)",
+            file: file,
+            line: line
+        )
     }
     
     private func runActivity(
